2955794985de6cc43fef2c177e140fef52bdd55d
Eoin Shanaghy <eoin.shanaghy@fourtheorem.com>
2018-11-01 09:50:07 +0000

    Add missing import for JsonIgnoreProperties

--- src/main/java/org/phoenixframework/channels/Envelope.java
+++ src/main/java/org/phoenixframework/channels/Envelope.java
@@ -2,6 +2,7 @@ package org.phoenixframework.channels;
 
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.JsonNode;
 
 // To fix UnrecognizedPropertyException.
